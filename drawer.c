5cd4730416152664e33b1b79266e403f93a9544b
venerjoel99 <84veneracionj@gmail.com>
2018-09-13 15:18:45 -0400

    Add files via upload

---
+++ drawer.c
@@ -0,0 +1,1 @@
+//File for drawing figures on the screen
---
+++ grapher.c
@@ -0,0 +1,18 @@
+
+void configureLighting(){
+}
+
+void configureViews(){
+}
+
+void setupGraphics(){
+    //Create a window
+    //Create cameras
+    //Position and orient cameras
+    //Create viewports
+    //Connect cameras to viewports
+    //Position viewports on the screen
+    //Create lighting for objects
+    //Keep black backgrounds
+}
+
